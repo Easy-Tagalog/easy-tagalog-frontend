2713bb8358bd17584d09a26c40843166083f63cc
Keanu C <keanuc@Keanus-MacBook-Pro.local>
2024-01-14 12:46:45 -0800

    Added initial translate page

--- src/app/(root)/translate/page.tsx
+++ src/app/(root)/translate/page.tsx
@@ -1,5 +1,22 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Search from "@/components/Search";
 import React from "react";
 
 export default function Translate() {
-  return <div>Translate</div>;
+  return (
+    <>
+      <MaxWidthWrapper className="flex flex-col justify-center items-center gap-4 mt-4">
+        <h2 className="text-3xl">Find Your English-Tagalog Translation</h2>
+        <p>
+          Translations include example phrases, audio clips, and emphasized
+          letters
+        </p>
+        <p>
+          Our dictionary continues to grow everyday, if you can't find the word
+          you're looking for, it may be added soon!
+        </p>
+        <Search />
+      </MaxWidthWrapper>
+    </>
+  );
 }
