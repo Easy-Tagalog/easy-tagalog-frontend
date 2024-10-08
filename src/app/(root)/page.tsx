101491dca52ab5ac719e3a1901c09f5ab9f08527
Keanu C <keanucaloua@gmail.com>
2024-10-07 18:38:06 -0700

    Added fonts and fixed homepage visually

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,157 +1,140 @@
-import Link from 'next/link';
-import { Righteous } from 'next/font/google';
+import Link from "next/link";
 
-import { MapPinned, Speech, BookOpen, HomeIcon } from 'lucide-react';
-import { buttonVariants } from '@/components/ui/button';
-import MaxWidthWrapper from '@/components/MaxWidthWrapper';
-import { cn } from '@/lib/utils';
+import { MapPinned, Speech, BookOpen, HomeIcon } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { cn } from "@/lib/utils";
 
 // max-w-prose is good for <p>
 
-const righteous = Righteous({ subsets: ['latin'], weight: '400' });
-
 const BULLET_INFO = [
   {
-    name: 'Learn Casual Tagalog',
+    name: "Learn Casual Tagalog",
     Icon: Speech,
     description:
-      'Although Easy Tagalog will teach a bit on how to speak formally, our goal is to have you speak like a normal Tagalog speaker and speak colloquial Tagalog.',
+      "Although Easy Tagalog will teach a bit on how to speak formally, our goal is to have you speak like a normal Tagalog speaker and speak colloquial Tagalog.",
   },
   {
-    name: 'Philippines Geography',
+    name: "Philippines Geography",
     Icon: MapPinned,
     description:
       "Each section and lesson will provide a little insight on different parts and provinces of the Philippines. While learning Tagalog, it's good to know some things about the country.",
   },
   {
-    name: 'Philippines Culture',
+    name: "Philippines Culture",
     Icon: HomeIcon,
     description:
-      'Every Tagalog speaker should be accustomed to some of the Filipino culture. That being traditions, holidays, clothing, and food.',
+      "Every Tagalog speaker should be accustomed to some of the Filipino culture. That being traditions, holidays, clothing, and food.",
   },
   {
-    name: 'Philippines History',
+    name: "Philippines History",
     Icon: BookOpen,
     description:
-      'The Philippines has a rich history aswell, and it might be good to know how Tagalog was influenced by other countries and their languages.',
+      "The Philippines has a rich history aswell, and it might be good to know how Tagalog was influenced by other countries and their languages.",
   },
 ];
 
 const EXPLORE_LESSONS = [
   {
-    name: 'Greetings',
+    name: "Greetings",
     Icon: Speech,
-    tagalog: 'Pagbati',
+    tagalog: "Pagbati",
   },
   {
-    name: 'Body',
+    name: "Body",
     Icon: Speech,
-    tagalog: 'Katawan',
+    tagalog: "Katawan",
   },
   {
-    name: 'Weather',
+    name: "Weather",
     Icon: Speech,
-    tagalog: 'Panahon',
+    tagalog: "Panahon",
   },
   {
-    name: 'Feelings',
+    name: "Feelings",
     Icon: Speech,
-    tagalog: 'Damdamin',
+    tagalog: "Damdamin",
   },
   {
-    name: 'Food',
+    name: "Food",
     Icon: Speech,
-    tagalog: 'Pagkain',
+    tagalog: "Pagkain",
   },
 ];
 
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper>
-        <div className="py-40 mx-auto text-center flex flex-col gap-8 items-center max-w-3xl">
-          <h1
-            className={cn(
-              'text-4xl tracking-tight text-gray-900 sm:text-6xl',
-              righteous.className
-            )}
-          >
-            Simplified{' '}
-            <span className="bg-gradient-to-r from-ph-blue via-ph-red to-ph-yellow text-transparent bg-clip-text">
-              Tagalog
-            </span>{' '}
-            Learning
+      <section className="h-[90vh]">
+        <MaxWidthWrapper className="flex flex-col h-full items-center justify-center gap-8 text-center">
+          <h1 className="font-fredoka flex flex-col">
+            <span className="text-[5rem] font-fraunces font-bold bg-gradient-to-r from-ph-blue via-ph-red to-ph-yellow text-transparent bg-clip-text sm:text-[7rem] md:text-[9rem] lg:text-[12rem]">
+              TAGALOG
+            </span>
+            <span className="text-lg font-semibold text-ph-blue md:text-xl">
+              Made Fun & Simple
+            </span>
           </h1>
 
-          <p className="text-lg text-muted-foreground">
-            Kamusta! Learn how to speak Tagalog the way native speakers do. This
-            website is still in beta, new content coming soon!
+          <p className="text-base text-muted-foreground md:text-lg">
+            Still in beta, new content coming soon!
           </p>
 
           <Link
             href="/learn"
-            className={buttonVariants({ variant: 'destructive' })}
+            className={buttonVariants({ variant: "destructive" })}
           >
             Start Learning
           </Link>
-        </div>
-      </MaxWidthWrapper>
+        </MaxWidthWrapper>
+      </section>
 
       {/* Bulletpoint section */}
-      <section className="bg-gray-100">
-        <MaxWidthWrapper className="py-20 px-4 md:px-0">
-          <div className="flex flex-col gap-10 md:gap-24">
-            {BULLET_INFO.map((info, i) => {
-              // This flips the icon and description
-              const isOdd = i % 2 !== 0;
+      <section className="bg-gray-100 py-4 h-screen">
+        <MaxWidthWrapper className="h-full flex flex-col justify-around sm:px-4 md:px-0">
+          {BULLET_INFO.map((info, i) => {
+            // This flips the icon and description
+            const isOdd = i % 2 !== 0;
 
-              return (
-                <div
-                  key={info.name}
-                  className={`text-left flex justify-around items-center max-w-6xl mx-auto gap-10 md:gap-20 ${
-                    isOdd && 'flex-row-reverse'
-                  }`}
-                >
-                  {/* This is the circle with icon */}
-                  <div className="md:flex-shrink-0 flex justify-center">
-                    <div className="h-24 w-24 flex items-center justify-center rounded-full bg-blue-100 text-ph-blue">
-                      {<info.Icon className="w-1/2 h-1/2" />}
-                    </div>
-                  </div>
-
-                  <div className="max-w-xl md:ml-4 lg:ml-0">
-                    <h3
-                      className={cn(
-                        'text-2xl font-medium',
-                        righteous.className
-                      )}
-                    >
-                      {info.name}
-                    </h3>
-                    <p className="mt-3 text-sm text-muted-foreground md:text-base">
-                      {info.description}
-                    </p>
+            return (
+              <div
+                key={info.name}
+                className={`text-left flex justify-around items-center max-w-6xl mx-auto md:gap-4 ${
+                  isOdd && "flex-row-reverse"
+                }`}
+              >
+                {/* This is the circle with icon */}
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-24 w-24 flex items-center justify-center rounded-full bg-blue-100 text-ph-blue">
+                    {<info.Icon className="w-1/2 h-1/2" />}
                   </div>
                 </div>
-              );
-            })}
-          </div>
+
+                <div className="max-w-xl p-4 md:ml-4 lg:ml-0">
+                  <h3 className="font-fraunces italic text-lg sm:text-xl">
+                    {info.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground md:text-base">
+                    {info.description}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
         </MaxWidthWrapper>
       </section>
 
       {/* Explore Lessons section */}
-      <section>
-        <MaxWidthWrapper className="py-20">
-          <h3 className={cn('text-2xl text-center', righteous.className)}>
-            Explore Lessons
-          </h3>
+      <section className="h-screen py-4">
+        <MaxWidthWrapper className="h-full flex flex-col justify-center items-center">
+          <h3 className="text-2xl text-center">Explore Lessons</h3>
 
-          <div className="py-10 gap-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+          <div className="py-10 gap-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             {EXPLORE_LESSONS.map((lessonData, i) => {
               return (
                 <div
                   key={i}
-                  className="flex flex-col mx-auto justify-around p-4 w-48 h-60 bg-ph-red text-ph-white rounded-md transition-all hover:cursor-pointer hover:bg-ph-blue hover:text-ph-yellow"
+                  className="flex flex-col mx-auto justify-around p-4 w-28 h-40 bg-ph-red text-ph-white rounded-md transition-all hover:cursor-pointer hover:bg-ph-blue hover:text-ph-yellow md:w-48 md:h-60"
                 >
                   <lessonData.Icon className="w-1/2 h-1/2 mx-auto" />
 
@@ -167,7 +150,7 @@ export default function Home() {
           <div className="flex justify-center">
             <Link
               href="/learn"
-              className={buttonVariants({ variant: 'destructive' })}
+              className={buttonVariants({ variant: "destructive" })}
             >
               Explore More Lessons
             </Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,34 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import MaxWidthWrapper from './MaxWidthWrapper';
-import Logo from './Logo';
-import SearchBar from './SearchBar';
-import MobileNav from './MobileNav';
-import AuthNav from './AuthNav';
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Logo from "./Logo";
+import SearchBar from "./TranslateSearchBar";
+import MobileNav from "./MobileNav";
+import AuthNav from "./AuthNav";
 
 export default function Navbar() {
   // Test user
   const user = null;
 
   return (
-    <header className="relative bg-white z-50 top-0 inset-x-0 h-16">
-      <MaxWidthWrapper>
-        <nav className="border-b border-gray-200">
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center gap-12">
-              <Link href="/" className="transition-all hover:text-red-600">
-                <Logo />
-              </Link>
+    <header className="relative bg-white h-[10vh] border-b border-gray-200">
+      <MaxWidthWrapper className="h-full">
+        <nav className="h-full flex items-center justify-between">
+          <div className="flex items-center gap-12">
+            <Link href="/" className="transition-all hover:text-red-600">
+              <Logo />
+            </Link>
 
-              <div className="hidden lg:block">
-                <SearchBar className="w-80" />
-              </div>
+            <div className="hidden lg:block">
+              <SearchBar className="w-80" />
             </div>
+          </div>
 
-            <div className="flex gap-4">
-              <MobileNav />
+          <div className="flex gap-4">
+            <MobileNav />
 
-              <div className="hidden lg:block">
-                {user === null && <AuthNav />}
-              </div>
+            <div className="hidden lg:block">
+              {user === null && <AuthNav />}
             </div>
           </div>
         </nav>
