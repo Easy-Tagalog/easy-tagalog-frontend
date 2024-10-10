d34d9ba45a114b480c6fa6abba3fae6173ce6917
keanu-a <keanucaloua@gmail.com>
2024-10-09 22:41:14 -0700

    Created section lesson cards

--- src/app/(root)/learn/[section]/page.tsx
+++ src/app/(root)/learn/[section]/page.tsx
@@ -1,26 +1,62 @@
 'use client';
 
+import LessonCard from '@/components/LessonCard';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import SectionLessonCard from '@/components/SectionLessonCard';
 
-const LESSONS_DATA = [
-  {
-    lessonTitle: 'Greetings',
-  },
-  {
-    lessonTitle: ' ',
-  },
-];
+const TEST_SECTION_LESSON_DATA = {
+  sectionName: 'Beginner: Foundation',
+  sectionLessons: [
+    {
+      lessonTitle: 'Alphabet',
+      href: '',
+      progress: 100,
+    },
+    {
+      lessonTitle: 'Simple Greetings',
+      href: '',
+      progress: 75,
+    },
+    {
+      lessonTitle: 'Personal Pronouns',
+      href: '',
+      progress: 25,
+    },
+    {
+      lessonTitle: 'Simple Phrases',
+      href: '',
+      progress: 0,
+    },
+  ],
+};
 
-interface SectionProps {
+interface ISectionProps {
   params: {
     section: string;
   };
 }
 
-export default function Section({ params }: SectionProps) {
+export default function Section({ params }: ISectionProps) {
   return (
-    <MaxWidthWrapper>
-      <h2>{params.section}</h2>
-    </MaxWidthWrapper>
+    <div className="mb-12">
+      <MaxWidthWrapper>
+        <h2 className="font-righteous text-3xl text-center py-12">
+          {TEST_SECTION_LESSON_DATA.sectionName}
+        </h2>
+
+        <div className="flex flex-col gap-8 items-center">
+          {TEST_SECTION_LESSON_DATA.sectionLessons.map(
+            ({ lessonTitle, href, progress }, sectionLessonIndex) => (
+              <SectionLessonCard
+                lessonTitle={lessonTitle}
+                progress={progress}
+                href={href}
+                key={sectionLessonIndex}
+              />
+            )
+          )}
+        </div>
+      </MaxWidthWrapper>
+    </div>
   );
 }
